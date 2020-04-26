7341a270a122017a5c27ef86907bc2e92b48c0d6
shenyanming <sym542569199@gmail.com>
2020-04-26 18:26:42 +0800

    分布式锁

---
+++ 分布式锁-curator/src/main/java/com/sym/CuratorApplication.java
@@ -0,0 +1,11 @@
+package com.sym;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ym.shen
+ * @date 2020/4/26 18:19.
+ */
+@SpringBootApplication
+public class CuratorApplication {
+}
